49d8c46f48e1cf4e5da965e894dfe8e92b531dfc
Cengiz Özdemir <cengizank18@gmail.com>
2023-02-16 12:21:14 +0300

    SecurityConfig

--- src/main/java/com/works/configs/SecurityConfig.java
+++ src/main/java/com/works/configs/SecurityConfig.java
@@ -1,25 +1,41 @@
 package com.works.configs;
 
+import com.works.services.AdminDetailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // spring security sınıfı pom dosysına eklendiğinde eğer bu yapı olmazsa erişim olmaz
+
+    final AdminDetailService adminDetailService;
+    final PasswordEncoder encoder;
 
     //Sql -> Rule
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        // spring tarafından beklenen formata çevrildi.
+        auth.userDetailsService(adminDetailService).passwordEncoder(encoder);
     }
-
 
     //Rule -> Mapping
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       http.csrf().disable().formLogin().disable();
+        // http basic ile autotacion istemek demek
+        // andMatcher kurallar demek hangi mapping hangi kuralı olacak demek
+        //** herşey demek
+        http.httpBasic()
+                .and().authorizeHttpRequests()
+                .antMatchers("/product/**").hasRole("product")
+                .antMatchers("/note/**").hasRole("note")
+                .and().
+                csrf().disable().formLogin().disable();
+
         // h2 console enable
         http.headers().frameOptions().disable();
     }
