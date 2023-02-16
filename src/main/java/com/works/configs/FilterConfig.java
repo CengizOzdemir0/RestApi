16e2ef8970a56262cc43a52f2c2f9f37a3a0135d
Cengiz Özdemir <cengizank18@gmail.com>
2023-02-16 15:25:04 +0300

    FilterConfig

--- src/main/java/com/works/configs/FilterConfig.java
+++ src/main/java/com/works/configs/FilterConfig.java
@@ -1,17 +1,26 @@
 package com.works.configs;
 
+import com.works.entities.Info;
+import com.works.repositories.InfoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class FilterConfig implements Filter {
+
+    final InfoRepository infoRepository;
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
@@ -23,8 +32,24 @@ public class FilterConfig implements Filter {
         String detail = "" + auth.getDetails();
         String agent = req.getHeader("user-agent");
         Long date = new Date().getTime();
-        System.out.println(url + " " + username + " " + detail + " " + agent + " " + date);
-        auth.getAuthorities().toArray();
+
+        // bi kullanıcının yekisi olmadığı durumlarda atılan isteklerde isteğin önünü kesmek için kullanılır.
+        List<String> roles = new ArrayList<>();
+        for(GrantedAuthority item : auth.getAuthorities()){
+            roles.add(item.getAuthority());
+        }
+        System.out.println(url + " "+roles+"" + username + " " + detail + " " + agent + " " + date);
+        if(!url.contains("h2-console")){
+        Info i = new Info();
+        i.setUrl(url);
+        i.setRoles(roles.toString());
+        i.setAgent(agent);
+        i.setDate(date);
+        i.setDetail(detail);
+        i.setUsername(username);
+
+        infoRepository.save(i);
+        }
 
         filterChain.doFilter(req, res);
 
