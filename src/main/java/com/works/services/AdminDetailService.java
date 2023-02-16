55b1c50e85af8e062f861bc88ed9288a59563fe1
Cengiz Özdemir <cengizank18@gmail.com>
2023-02-16 10:43:10 +0300

    Admin

---
+++ src/main/java/com/works/services/AdminDetailService.java
@@ -0,0 +1,18 @@
+package com.works.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminDetailService implements UserDetailsService {
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+
+
+}
